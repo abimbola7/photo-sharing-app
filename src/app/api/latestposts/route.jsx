a9421b6df154b8f3d88001232601d7317bc71a5c
abimbola7 <abimbolababajide6@gmail.com>
2024-02-16 13:52:46 +0100

    commit

--- src/app/api/latestposts/route.jsx
+++ src/app/api/latestposts/route.jsx
@@ -3,7 +3,8 @@ import { Post } from "@/app/(models)/user";
 
 export async function GET(req) {
   try {
-    const latest  = await Post.find().sort({createdAt: -1}).limit(5)
+    const latest  = await Post.find().sort({createdAt: -1})
+    // .limit(5)
     return NextResponse.json({ latest }, { status : 200 })
   } catch(error){
     return NextResponse.json({ message : error }, { status : 500 })
