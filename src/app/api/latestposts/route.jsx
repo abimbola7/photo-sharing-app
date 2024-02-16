5efbdabedd8b4e81c3e7775e8598236475ec2cc5
abimbola7 <abimbolababajide6@gmail.com>
2024-02-16 14:49:38 +0100

    limit fix

--- src/app/api/latestposts/route.jsx
+++ src/app/api/latestposts/route.jsx
@@ -4,8 +4,7 @@ import { Post } from "@/app/(models)/user";
 export async function GET(request) {
   console.log(request)
   try {
-    const latest  = await Post.find().sort({createdAt: -1})
-    // .limit(5)
+    const latest  = await Post.find().sort({createdAt: -1}).limit(5)
     return NextResponse.json({ latest }, { status : 200 })
   } catch(error){
     return NextResponse.json({ message : error }, { status : 500 })
